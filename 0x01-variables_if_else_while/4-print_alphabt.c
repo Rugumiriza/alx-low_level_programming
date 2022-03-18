f03514cf53cc47ae71e33ade896fb8e2aa54e25c
Rugumiriza <rugumirizah@gmail.com>
2022-03-18 05:32:27 -0700

    tsdk 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet except the letters q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+		while (ch <= 'z')
+		{
+			if (ch != 'e' && ch != 'q')
+			{
+				putchar(ch);
+			}
+			ch++;
+		}
+		putchar('\n');
+	return (0);
+}
