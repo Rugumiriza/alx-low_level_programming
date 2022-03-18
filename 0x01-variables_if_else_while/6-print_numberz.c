9be76ab67dc1224980a77b09b379d08923c639cb
Rugumiriza <rugumirizah@gmail.com>
2022-03-18 05:41:06 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int i = '0';
+
+		while (i <= '9')
+		{
+			putchar(i);
+			i++;
+		}
+		putchar('\n');
+	return (0);
+}
