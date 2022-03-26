befaa7aba05aa1033f02acc99b4328bac72316f2
Rugumiriza <rugumirizah@gmail.com>
2022-03-26 10:14:01 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: is an integer
+ * @b: is another integer
+ * Return: 1 on success
+ */
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+		return (mult);
+}
