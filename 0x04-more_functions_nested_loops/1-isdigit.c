39566afcf0532511732d19e21f6cd97ee3c13f10
Rugumiriza <rugumirizah@gmail.com>
2022-03-26 09:54:47 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,11 @@
 /**
  * _isdigit - checks for an integer
  * @c: ascii value
- * Return: 1 on success 
+ * Return: 1 on success
  */
 int _isdigit(int c)
 {
-	if ( c >= 48 && c <= 57 )
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
