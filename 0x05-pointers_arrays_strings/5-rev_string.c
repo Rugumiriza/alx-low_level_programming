bcc21b778416fd680c6c24b2d5ebc09392b82892
Rugumiriza <100429998+Rugumiriza@users.noreply.github.com>
2022-03-31 02:45:59 +1400

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+ */
+void rev_string(char *s)
+{
+
+	int i, j, k;
+	char z;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	j = 0;
+
+	k = i / 2;
+
+	while (k--)
+	{
+		z = s[i - j - 1];
+		s[i - j - 1] = s[j];
+		s[j] = z;
+		j++;
+	}
+
+}
